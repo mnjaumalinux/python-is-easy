e743934d3816fdbbfef37757410a3fc9849e01f8
mnjau9 <mnjau9@gmail.com>
2018-12-10 17:38:48 +0300

    Homewor#1 Variables

---
+++ variables/main.py
@@ -0,0 +1,43 @@
+"""
+This song attributes describe mainly sound characteristic.
+Alll these is a adescription of my favourite song that i listen to more often.
+
+"""
+
+
+Song_title = "Childs Play" # Describes the title of the song
+Artist	= "Drake"
+Genre	= "pop"
+Speechiness = 0.156 # 
+Loudness = -11.204 # The higher the value, the louder the song (in dB).
+Acousticness = 0.0145 # The higher the value the more acoustic the song is.
+Tempo = 80.03
+Danceability = 1 #The higher the value, the easier it is to dance to this song.
+Valence = 0.308 #The higher the value, the more positive mood for the song.
+LengthInSeconds = 241400 #The duration of the song.
+Popularity = "medium"
+
+
+print(Song_title)
+print(Artist)
+print(Genre)
+print(Speechiness)
+print(Loudness)
+print(Acousticness)
+print(Tempo)
+print(Danceability)
+print(Valence)
+print(LengthInSeconds)
+print(Popularity)
+
+
+
+
+
+
+
+
+
+
+
+
