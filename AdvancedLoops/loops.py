a5e2704530de16db678ea520a453d3086a34c0d2
mnjau9 <mnjau9@gmail.com>
2018-12-13 15:35:26 +0300

    Homework Assignment #6: Advanced Loops

---
+++ AdvancedLoops/loops.py
@@ -0,0 +1,25 @@
+"""
+This function takes in rows and columns parameters and draws a playing board with the specifie parameters.
+"""
+
+def PlayingBoard(rows,columns):
+	rows = int(rows)
+	columns = int(columns)		
+	for num in range(rows):
+		if rows > 21 or columns > 21:
+			return False	# if the board is larger than 21 rows or 21 columns break out of the loop
+			break		
+
+		if num%2 == 0:					
+			print(" | "*columns)
+		else:
+			print("---"*rows)
+
+	
+
+
+
+
+
+PlayingBoard(20,20)
+
